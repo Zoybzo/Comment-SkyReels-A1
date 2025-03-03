8582d1ec4e33988d2521a70d78d55eaf6b8b9cae
Mkpk <user@BJ206116.local>
2025-03-03 14:47:15 +0800

    Fixed device error in Test

--- tests/mobilenetv3.py
+++ tests/mobilenetv3.py
@@ -10,16 +10,18 @@ from skyreels_a1.src.smirk_encoder import SmirkEncoder
 
 
 def test_smirk_encoder():
+    device = torch.device('cuda')
     smirk_encoder = SmirkEncoder()
+    smirk_encoder.to(device)
     loguru_logger.log('UNIT_DEBUG', "Load Pose Encoder")
     pose_encoder = smirk_encoder.pose_encoder
-    summary(pose_encoder, (3, 224, 224))
+    summary(pose_encoder, (3, 224, 224), device='cuda')
     loguru_logger.log('UNIT_DEBUG', "Load Pose Encoder")
     expression_encoder = smirk_encoder.expression_encoder
-    summary(expression_encoder, (3, 224, 224))
+    summary(expression_encoder, (3, 224, 224), device='cuda')
     loguru_logger.log('UNIT_DEBUG', "Load Pose Encoder")
     shape_encoder = smirk_encoder.shape_encoder
-    summary(shape_encoder, (3, 224, 224))
+    summary(shape_encoder, (3, 224, 224), device='cuda')
 
 
 if __name__ == '__main__':
