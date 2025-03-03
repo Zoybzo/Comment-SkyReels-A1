f967abc5073e867a78407c2b37a3db3cb1155493
Mkpk <user@BJ206116.local>
2025-03-03 16:07:02 +0800

    Added logger

--- inference.py
+++ inference.py
@@ -29,6 +29,7 @@ from skyreels_a1.src.media_pipe.draw_util_2d import FaceMeshVisualizer2d
 
 
 def customize_loguru_logger():
+    # UNIT_DEBUG
     loguru_logger.level("UNIT_DEBUG", no=16, color="<green>",
                         icon="🧪")  # 单元测试的调试信息
     loguru_logger.add("logs/unit_debug.log",
@@ -36,6 +37,14 @@ def customize_loguru_logger():
                                                 "level"].name == "UNIT_DEBUG",
                       level="UNIT_DEBUG")
     loguru_logger.log("UNIT_DEBUG", "This is a unit test debug message.")
+    # MODEL DEBUG
+    loguru_logger.level("MODEL_DEBUG", no=15, color="<blue>",
+                        icon="🤖")  # 测试模型的调试信息
+    loguru_logger.add("model_debug.log",
+                      filter=lambda record: record[
+                                                "level"].name == "MODEL_DEBUG",
+                      level="MODEL_DEBUG")
+    loguru_logger.log("MODEL_DEBUG", "This is a model debug message.")
 
 
 def crop_and_resize(image, height, width):
