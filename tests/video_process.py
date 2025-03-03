cc9b7223b4137b9f672887c4a30599da3fcb623e
Mkpk <user@BJ206116.local>
2025-03-03 10:58:49 +0800

    Test

--- tests/video_process.py
+++ tests/video_process.py
@@ -1,10 +1,10 @@
 from insightface.app import FaceAnalysis
 from decord import VideoReader
-from ..inference import parse_video
+from inference import parse_video
 from skyreels_a1.pre_process_lmk3d import FaceAnimationProcessor
 
 
-def test_video(video_path='../assets/driving_video/1.mp4', max_frame_num=49):
+def test_video(video_path='assets/driving_video/1.mp4', max_frame_num=49):
     processor = FaceAnimationProcessor(
         checkpoint='pretrained_models/smirk/SMIRK_em1.pt')
     control_frames = parse_video(video_path, max_frame_num)
