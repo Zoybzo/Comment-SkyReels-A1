ed0f40985a5a4d278dcf7fa55186d2c9f05e8bd6
Mkpk <user@BJ206116.local>
2025-03-04 10:54:45 +0800

    c

--- inference.py
+++ inference.py
@@ -319,20 +319,20 @@ if __name__ == "__main__":
     loguru_logger.info("Loading CogVideoX...")
     transformer = CogVideoXTransformer3DModel.from_pretrained(
         model_name,
-        subfolder="transformer",
+        subfolder="transformer", load_in_8bit=True,
         # device_map="auto" # CogVideoX 不支持 auto
     ).to(weight_dtype)
 
     loguru_logger.info("Loading VAE...")
     vae = AutoencoderKLCogVideoX.from_pretrained(
         model_name,
-        subfolder="vae", device_map="balanced"
+        subfolder="vae", device_map="balanced", load_in_8bit=True,
     ).to(weight_dtype)
 
     loguru_logger.info("Loading lmk_encoder...")
     lmk_encoder = AutoencoderKLCogVideoX.from_pretrained(
         model_name,
-        subfolder="pose_guider", device_map="balanced"
+        subfolder="pose_guider", device_map="balanced", load_in_8bit=True,
     ).to(weight_dtype)
 
     loguru_logger.info("Loading SkyReels...")
@@ -345,7 +345,7 @@ if __name__ == "__main__":
         feature_extractor=siglip_normalize,
         torch_dtype=torch.bfloat16,
         # device_map="auto",
-        device_map="balanced",
+        device_map="balanced", load_in_8bit=True,
     )
 
     pipe.to("cuda")
