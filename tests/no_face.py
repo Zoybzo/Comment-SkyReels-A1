00ea3d0c6c6476886f3ad6c0811ccef1f8226d40
Mkpk <user@BJ206116.local>
2025-03-04 14:27:55 +0800

    c

--- tests/no_face.py
+++ tests/no_face.py
@@ -1,20 +1,19 @@
 from insightface.app import FaceAnalysis
 from decord import VideoReader
+from PIL import Image
+import numpy as np
+
 from inference import parse_video, customize_loguru_logger
 from skyreels_a1.pre_process_lmk3d import FaceAnimationProcessor
 
 
-def test_video(video_path='assets/driving_video/1.mp4', max_frame_num=49):
+def test_no_face(image_path='assets/test_images/1.jpg'):
     processor = FaceAnimationProcessor(
         checkpoint='pretrained_models/smirk/SMIRK_em1.pt')
-    control_frames = parse_video(video_path, max_frame_num)
-    # driving video crop face
-    driving_video_crop = []
-    for control_frame in control_frames:
-        frame, _, _ = processor.face_crop(control_frame)
-        driving_video_crop.append(frame)
+    img = Image.open(image_path).convert('RGB')
+    frame, _, _ = processor.face_crop(np.array(img))
 
 
 if __name__ == '__main__':
     customize_loguru_logger()
-    test_video()
+    test_no_face()
